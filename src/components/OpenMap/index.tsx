f15f7f806731c415b8cfe53d6b6f7ef98bc20b5a
caixin <1137089297@qq.com>
2025-04-09 14:32:49 +0800

    加载map

---
+++ src/components/OpenMap/index.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from 'mapbox-gl-geocoder';
+
+// 替换为你的 Mapbox Access Token
+mapboxgl.accessToken = 'pk.eyJ1IjoieWFuZ2ppYW4iLCJhIjoiY2phaG1neno0MXFkNDMzbWhwNWw0bWM4aiJ9.CFmrh0LVWAbmVeed-Xr7wA';
+
+let map;
+const MapComponent = () => {
+  const mapContainerRef = useRef(null);
+  function rotate() {
+    let center = map.getCenter();
+    map.easeTo({
+        center: [center.lng + 40, center.lat],
+        zoom: 2,
+        speed: 0.5
+    })
+    requestAnimationFrame(rotate);
+}
+  useEffect(() => {
+    map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-74.5, 40], // 初始中心点坐标
+        zoom: 9, // 初始缩放级别
+      projection: 'globe' // 坐标系
+    });
+
+    // 添加控制条
+    map.addControl(new mapboxgl.NavigationControl());
+    map.flyTo({
+        center: [112.92850, 28.18],
+        zoom: 14,
+        speed: 0.4,
+        pitch: 60,
+    })
+    map.on('load', () => {
+        map.addSource('mapbox-dem', {
+        'type': 'raster-dem',
+        'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+        'tileSize': 512,
+        'maxzoom': 14
+        });
+        map.addControl(new mapboxgl.FullscreenControl());
+map.addControl(new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true
+}));
+map.addControl(new mapboxgl.ScaleControl({
+    maxWidth: 200,
+    unit: 'metric'
+}), 'bottom-left');
+map.addControl(new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+}), 'top-left');
+map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+
+        map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+    });
+    setTimeout(() => {
+        rotate();
+    },20000)
+    // 清理地图实例
+    return () => map.remove();
+  }, []);
+
+  return <div ref={mapContainerRef} style={{ width: '100%', height: '500px' }} />;
+};
+
+export default MapComponent;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,20 @@
+import Guide from '@/components/Guide';
+import OpenMap from '@/components/OpenMap';
+import { trim } from '@/utils/format';
+import { PageContainer } from '@ant-design/pro-components';
+import { useModel } from '@umijs/max';
+import styles from './index.less';
+
+const HomePage: React.FC = () => {
+  const { name } = useModel('global');
+  return (
+    <PageContainer ghost>
+      <div className={styles.container}>
+        <Guide name={trim(name)} />
+        <OpenMap/>
+      </div>
+    </PageContainer>
+  );
+};
+
+export default HomePage;
